e62898f131d7441e9ed5ab6370fff23f6743f40d
imstephtp <cstephany.tp@gmail.com>
2023-02-09 17:42:46 -0600

    Ejercicios jueves

---
+++ IntroWeb7/js01/tiposdedatosyvariables.js
@@ -0,0 +1,12 @@
+var nombre="Stephany"
+var edad=27
+var tengoMascotas= true
+var tengoTrabajo= false
+var tengoSueldo= //undefined
+
+let nombre= "Stephany"
+let edad=27
+let tengoMascotas= true
+let tengoTrabajo= false
+let tengoSueldo= //undefined
+
