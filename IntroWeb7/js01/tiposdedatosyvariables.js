431d3c371a00d42e663128dc923db94eebaca894
alberto romano <aromanog1@gmail.com>
2023-02-10 01:18:15 +0100

    Hola cariño

--- IntroWeb7/js01/tiposdedatosyvariables.js
+++ IntroWeb7/js01/tiposdedatosyvariables.js
@@ -1,12 +1,11 @@
-var nombre="Stephany"
-var edad=27
-var tengoMascotas= true
-var tengoTrabajo= false
-var tengoSueldo= //undefined
+var nombre = 'Stephany';
+var edad = 27;
+var tengoMascotas = true;
+var tengoTrabajo = false;
+var tengoSueldo = undefined;
 
-let nombre= "Stephany"
-let edad=27
-let tengoMascotas= true
-let tengoTrabajo= false
-let tengoSueldo= //undefined
-
+let nombre = 'Stephany';
+let edad = 27;
+let tengoMascotas = true;
+let tengoTrabajo = false;
+let tengoSueldo = undefined;
