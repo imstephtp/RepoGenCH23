367ec94df6b2ea92c791f121885e76094306da9d
Stephany Trujillo <cstephany.tp@gmail.com>
2023-03-27 10:14:08 -0600

    jap

--- JAVA-Web/jw-01-sts/ormJpa/src/main/java/com/generation/cohorte23/jpa/controllers/UsuarioController.java
+++ JAVA-Web/jw-01-sts/ormJpa/src/main/java/com/generation/cohorte23/jpa/controllers/UsuarioController.java
@@ -1,9 +1,12 @@
 package com.generation.cohorte23.jpa.controllers;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +21,16 @@ import com.generation.cohorte23.jpa.services.UsuarioServices;
 public class UsuarioController {
 	@Autowired
 	UsuarioServices usuarioService;
+	
+	@DeleteMapping(path = "/{id}")
+	public String eliminarPorId(@PathVariable("id")Long id) {
+		boolean ok = usuarioService.eliminarUsuario(id);
+		if (ok) {
+			return "Se eliminó el usuario";
+		} else {
+			return "No se eliminó el usuario";
+		}
+	}
 
 	@GetMapping()
 	public ArrayList<UsuarioModel> ListarUsuarios() {
@@ -33,5 +46,8 @@ public class UsuarioController {
 	public ArrayList<UsuarioModel> obtenerPorPrioridad(@RequestParam("prioridad") Integer prioridad) {
 		return usuarioService.busquedaPorPrioridad(prioridad);
 	}
-
+	@GetMapping(path ="/{id}")
+	public Optional<UsuarioModel> obtenerUsuarioPorId(@PathVariable ("id") Long id){
+		return usuarioService.obtenerPorId(id);
+	}
 }
